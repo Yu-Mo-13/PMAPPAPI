a9018dbfe4571fb21c5f61d7310c14c10fe47760
Yuya Motozono <motozono.tech@gmail.com>
2023-12-23 11:10:46 +0900

    CORSエラー対応

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from routers.application import router as application_router
 from routers.password import router as password_router
+from starlette.middleware.cors import CORSMiddleware
 # from stargql import GraphQL
 
 # from database.database import db_session
 # from schema.schema import schema
 
 app = FastAPI()
+
+# CORSエラーを回避するための設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 # app.add_route("/graphql", GraphQL(schema=schema, graphiql=True))
 
